c0c3ee4e3eb5140b770a0a197084e299a7b134b9
NightZero <nightzero@gmail.com>
2017-03-25 20:40:07 +0100

    updates

---
+++ paxa-java-rest/src/main/java/se/hexabit/paxa/rest/types/User.java
@@ -0,0 +1,12 @@
+package se.hexabit.paxa.rest.types;
+
+/**
+ * Created by nightzero on 2017-03-20.
+ */
+public class User {
+    private long userId;
+    private String userName;
+    private String firstName;
+    private String eMail;
+    private String role; //Admin or Booker
+}
