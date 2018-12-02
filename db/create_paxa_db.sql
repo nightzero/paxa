154e194c2472b8d3e4d5a18ad837aa1f10ca2e85
nightzero <nightzero@gmail.com>
2018-12-02 21:19:18 +0100

    Updating datamodel with categories

--- db/create_paxa_db.sql
+++ db/create_paxa_db.sql
@@ -1,6 +1,6 @@
 ﻿-- --------------------------------------------------------
 -- Värd:                         127.0.0.1
--- Server version:               5.7.17-log - MySQL Community Server (GPL)
+-- Server version:               5.7.18-log - MySQL Community Server (GPL)
 -- Server OS:                    Win64
 -- HeidiSQL Version:             9.4.0.5125
 -- --------------------------------------------------------
@@ -28,51 +28,70 @@ CREATE TABLE IF NOT EXISTS `bookings` (
   KEY `userFK` (`user_id`),
   CONSTRAINT `resFK` FOREIGN KEY (`resource_id`) REFERENCES `resources` (`id`) ON UPDATE CASCADE,
   CONSTRAINT `userFK` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8;
 
 -- Dumpar data för tabell paxa.bookings: ~0 rows (approximately)
 /*!40000 ALTER TABLE `bookings` DISABLE KEYS */;
 /*!40000 ALTER TABLE `bookings` ENABLE KEYS */;
 
+-- Dumping structure for tabell paxa.resourcecategory
+CREATE TABLE IF NOT EXISTS `resourcecategory` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` tinytext NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+-- Dumpar data för tabell paxa.resourcecategory: ~5 rows (approximately)
+/*!40000 ALTER TABLE `resourcecategory` DISABLE KEYS */;
+INSERT IGNORE INTO `resourcecategory` (`id`, `name`) VALUES
+	(1, 'BCD'),
+	(2, 'Båtar'),
+	(3, 'Flaskor'),
+	(4, 'Regulatorer'),
+	(5, 'Övrigt');
+/*!40000 ALTER TABLE `resourcecategory` ENABLE KEYS */;
+
 -- Dumping structure for tabell paxa.resources
 CREATE TABLE IF NOT EXISTS `resources` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` tinytext,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+  `name` tinytext NOT NULL,
+  `category` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_resources_resourcecategory` (`category`),
+  CONSTRAINT `FK_resources_resourcecategory` FOREIGN KEY (`category`) REFERENCES `resourcecategory` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8;
 
--- Dumpar data för tabell paxa.resources: ~8 rows (approximately)
+-- Dumpar data för tabell paxa.resources: ~28 rows (approximately)
 /*!40000 ALTER TABLE `resources` DISABLE KEYS */;
-INSERT IGNORE INTO `resources` (`id`, `name`) VALUES
-	(1, 'Brum-Brum'),
-	(2, 'Svarten'),
-	(3, 'Oxybox kvadratisk'),
-	(4, 'Regulatorpaket1'),
-	(5, 'Regulatorpaket2'),
-	(6, 'Regulatorpaket3'),
-	(7, 'Regulatorpaket4'),
-	(8, 'Regulatorpaket5'),
-	(9, 'Oxybox cylinder'),
-	(10, 'Väst 1 Balance(L)'),
-	(11, 'Väst 2 Balance(L)'),
-	(12, 'Väst 3 Balance(XL)'),
-	(13, 'Väst 4 Balance(XL)'),
-	(14, 'Väst 5 Balance(XL)'),
-	(15, 'Väst 6 Scubapro(XS)'),
-	(16, 'Väst 7 Scubapro(XS)'),
-	(17, 'Väst 8 Scubapro(ML)'),
-	(18, 'Väst 9 Scubapro(ML)'),
-	(19, 'Väst 10 Poseidon (M)'),
-	(20, 'Moby Dick'),
-	(21, 'Flaska 1 (10x300)'),
-	(22, 'Flaska 2 (10x300)'),
-	(23, 'Flaska 3 (10x300)'),
-	(24, 'Flaska 4 (10x300)'),
-	(25, 'Flaska 5 (10x300)'),
-	(26, 'Flaska 6 (10x300)'),
-	(27, 'Flaska 7 (10x300)'),
-	(28, 'Flaska 8 (8x300)');
-
+INSERT IGNORE INTO `resources` (`id`, `name`, `category`) VALUES
+	(1, 'Brum-Brum', 2),
+	(2, 'Svarten', 2),
+	(3, 'Oxybox', 5),
+	(4, 'Regulatorpaket1', 4),
+	(5, 'Regulatorpaket2', 4),
+	(6, 'Regulatorpaket3', 4),
+	(7, 'Regulatorpaket4', 4),
+	(8, 'Regulatorpaket5', 4),
+	(9, 'Oxybox cylinder', 5),
+	(10, 'Väst 1 Balance(L)', 1),
+	(11, 'Väst 2 Balance(L)', 1),
+	(12, 'Väst 3 Balance(XL)', 1),
+	(13, 'Väst 4 Balance(XL)', 1),
+	(14, 'Väst 5 Balance(XL)', 1),
+	(15, 'Väst 6 Scubapro(XS)', 1),
+	(16, 'Väst 7 Scubapro(XS)', 1),
+	(17, 'Väst 8 Scubapro(ML)', 1),
+	(18, 'Väst 9 Scubapro(ML)', 1),
+	(19, 'Väst 10 Poseidon (M)', 1),
+	(20, 'Moby Dick', 2),
+	(21, 'Flaska 1 (10x300)', 3),
+	(22, 'Flaska 2 (10x300)', 3),
+	(23, 'Flaska 3 (10x300)', 3),
+	(24, 'Flaska 4 (10x300)', 3),
+	(25, 'Flaska 5 (10x300)', 3),
+	(26, 'Flaska 6 (10x300)', 3),
+	(27, 'Flaska 7 (10x300)', 3),
+	(28, 'Flaska 8 (8x300)', 3);
 /*!40000 ALTER TABLE `resources` ENABLE KEYS */;
 
 -- Dumping structure for tabell paxa.users
@@ -83,10 +102,8 @@ CREATE TABLE IF NOT EXISTS `users` (
   `email` tinytext,
   PRIMARY KEY (`id`),
   UNIQUE KEY `profileid` (`profileid`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
 
--- Dumpar data för tabell paxa.users: ~2 rows (approximately)
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
